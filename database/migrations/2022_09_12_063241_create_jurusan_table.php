0d66eab6f63e1bf2e18ff15b9c470c5b59150ade
marsyanadia <deswitamn@student.ub.ac.id>
2022-09-12 13:50:12 +0700

    Update 2022_09_12_063241_create_jurusan_table.php

--- database/migrations/2022_09_12_063241_create_jurusan_table.php
+++ database/migrations/2022_09_12_063241_create_jurusan_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('jurusan', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('ID_Jurusan');
+            $table->string('Nama_Jurusan');
         });
     }
 
