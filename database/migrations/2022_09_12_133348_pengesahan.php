4acb719953dbff64952dab9ead382d170130d989
putriraiss <putridaliana@gmail.com>
2022-09-12 22:09:30 +0700

    Update 2022_09_12_133348_pengesahan.php

--- database/migrations/2022_09_12_133348_pengesahan.php
+++ database/migrations/2022_09_12_133348_pengesahan.php
@@ -14,15 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('pengesahan', function (Blueprint $table) {
-            $table->string('Dosen_Pengampu')->primary;
-            $table->foreignId('Mata_Kuliah_Kode_MK');
-            $table->foreignId('RPS_Kode_RPS');
+            $table->bigIncrements('Dosen_Pengampu')->primary;
+            $table->unsignedBigInteger('Mata_Kuliah_Kode_MK')->nullable();
+            $table->unsignedBigInteger('RPS_Kode_RPS')->nullable();
             $table->string('Status_2');
             $table->string('KPS');
             $table->string('KKJFD');
 
-            $table->foreign('Mata_Kuliah_Kode_MK')->references('Kode')->on('Mata_Kuliah');
-            $table->foreign('RPS_Kode_RPS')->references('Kode')->on('RPS');
+            $table->foreign('Mata_Kuliah_Kode_MK')->references('Kode_MK')->on('mata_kuliah');
+            $table->foreign('RPS_Kode_RPS')->references('Kode_RPS')->on('rps');
         });
     }
 
