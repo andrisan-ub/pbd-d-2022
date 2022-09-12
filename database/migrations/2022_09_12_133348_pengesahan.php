521e08c581065203429b789aa14af05b1a83e098
fauzanzakaria <fauzanzakaria449@gmail.com>
2022-09-12 20:59:51 +0700

    Pengesahan

---
+++ database/migrations/2022_09_12_133348_pengesahan.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengesahan', function (Blueprint $table) {
+            $table->string(Dosen_Pengampu)->primary;
+            $table->foreignId(Mata_Kuliah_Kode_MK);
+            $table->foreignId(RPS_Kode_RPS);
+            $table->string(Status_2);
+            $table->string(KPS);
+            $table->string(KKJFD);
+
+            $table->foreign('Mata_Kuliah_Kode_MK')->references('Kode')->on('Mata_Kuliah');
+            $table->foreign('RPS_Kode_RPS')->references('Kode')->on('RPS');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengesahan');
+    }
+};
