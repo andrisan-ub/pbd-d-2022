78474bedeccfc213bc4cb6296cd5a61b42f3ffe7
Nazwa Annila <112674903+annilacuaa@users.noreply.github.com>
2022-09-12 19:10:59 +0700

    add table mata_kuliah

---
+++ database/migrations/2022_09_12_120330_mata_kuliah.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->bigIncrements('kode');
+            $table->char('nama_matkul',40);
+            $table->integer('sks');
+            $table->unsignedBigInteger('NIM');
+            $table->foreign('NIM')->references('NIM')->on('Mahasiswa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+};
