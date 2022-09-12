f436f5b8ee1791757170a0bf872ddff1b2e4d9e1
alviantaa <112466435+alviantaa@users.noreply.github.com>
2022-09-12 20:44:39 +0700

    memperbaiki nama tabel

--- database/migrations/Mata_Kuliah_tabel.php
+++ database/migrations/Mata_Kuliah_tabel.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('Mahasiswa', function (Blueprint $table) {
+        Schema::create('Mata_Kuliah', function (Blueprint $table) {
             $table->int('ID_MK');
             $table->string('Nama_MK');
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Mahasiswa');
+        Schema::dropIfExists('Mata_Kuliah');
     }
 };
