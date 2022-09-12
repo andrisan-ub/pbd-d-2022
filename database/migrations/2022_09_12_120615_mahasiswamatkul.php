d75a07566fff5429a242b668ef11ae732645c77e
zauhairjubs <zauhairdy@gmail.com>
2022-09-12 19:10:46 +0700

    Create 2022_09_12_120615_mahasiswamatkul.php

---
+++ database/migrations/2022_09_12_120615_mahasiswamatkul.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswamatkul', function (Blueprint $table) {
+            $table->string('nim');
+            $table->foreignId('id_matkul');
+            $table->timestamps();
+
+            $table->foreign('id_matkul')->references('id')->on('matakuliah');
+            $table->foreign('nim')->references('nim')->on('mahasiswa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswamatkul');
+    }
+};
