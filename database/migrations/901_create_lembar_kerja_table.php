1cb16a213688c7608e3ce3b093aa6ea0475a2477
Naufal Ammar Rauf <naufalammarrauf7@gmail.com>
2022-09-12 22:00:32 +0700

    menambah foreign key

--- database/migrations/901_create_lembar_kerja_table.php
+++ database/migrations/901_create_lembar_kerja_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('lembar_kerja', function (Blueprint $table) {
             $table->string('ID_Lembar_Kerja')->primary;
-            $table->unsignedBigInteger('Sub_CPMK_ID_Sub_CPMK')->nullable();
+            $table->string('Sub_CPMK_ID_Sub_CPMK')->nullable();
             $table->foreign('Sub_CPMK_ID_Sub_CPMK')->references('sub_cpmk')->on('sub_cpmk');
         });
     }
