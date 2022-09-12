5802f43f31d82f54cd39278b154d0e071e6b069c
alviantaa <112466435+alviantaa@users.noreply.github.com>
2022-09-12 20:41:17 +0700

    MATA_KULIAH tabel

---
+++ database/migrations/Mata_Kuliah_tabel.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Mahasiswa', function (Blueprint $table) {
+            $table->int('ID_MK');
+            $table->string('Nama_MK');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Mahasiswa');
+    }
+};
