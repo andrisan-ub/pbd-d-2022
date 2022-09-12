7587dab81993cce3458a913da1c1c8fd216bdaf2
Naufal Ammar Rauf <naufalammarrauf7@gmail.com>
2022-09-12 18:45:38 +0700

    Update 2022_09_12_093741_create_mahasiswa_table.php

--- database/migrations/2022_09_12_093741_create_mahasiswa_table.php
+++ database/migrations/2022_09_12_093741_create_mahasiswa_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('mahasiswa', function (Blueprint $table) {
-            $table->id('NIM')->primary;
+            $table->integer('NIM')->primary;
             $table->string('Nama_mahasiswa');
             
         });
