f42a1a58499e8cf7c54ff2724a2d1131b6fa0201
alviantaa <112466435+alviantaa@users.noreply.github.com>
2022-09-12 20:40:46 +0700

    menghapus code tidak berguna

--- database/migrations/Mahasiswa_tabel.php
+++ database/migrations/Mahasiswa_tabel.php
@@ -4,10 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-Schema::dropIfExists('personal_access_tokens');
-Schema::dropIfExists('failed_jobs');
-Schema::dropIfExists('password_resets');
-Schema::dropIfExists('users');
 return new class extends Migration
 {
     /**
