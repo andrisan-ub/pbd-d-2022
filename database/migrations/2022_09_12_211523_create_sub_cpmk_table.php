7ce168734cfee0d979fa159af20a1236d1d58df0
claireYng <claraclaritayng@gmail.com>
2022-09-13 04:20:39 +0700

    Create 2022_09_12_211523_create_sub_cpmk_table.php

---
+++ database/migrations/2022_09_12_211523_create_sub_cpmk_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_cpmk', function (Blueprint $table) {
+            $table->bigIncrements('Kode_Sub_CPMK')->primary;
+	        $table->integer('Bobot_Sub_CPMK');
+	        $table->integer('UAS');
+	        $table->integer('UTS');
+	        $table->integer('LK_1');
+	        $table->integer('LK_2');
+	        $table->integer('LK_3');
+	        $table->integer('LK_4');
+
+	        $table->unsignedBigInteger('Penilaian_Kode_Penilaian')->nullable();
+	        $table->foreign('Penilaian_Kode_Penilaian')->references('Kode_Penilaian')->on('penilaian');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_cpmk');
+    }
+};
