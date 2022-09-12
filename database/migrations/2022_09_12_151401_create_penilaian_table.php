46f73dcc2bf8305ce2e39b31804ca8250240732d
jasmineffendy <103312975+jasmineffendy@users.noreply.github.com>
2022-09-12 23:44:18 +0800

    Create 2022_09_12_151401_create_penilaian_table.php

---
+++ database/migrations/2022_09_12_151401_create_penilaian_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penilaian', function (Blueprint $table) {
+            $table->bigIncrements('Kode_Penilaian')->primary;
+            $table->string('Predikat');
+            $table->integer('Nilai');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penilaian');
+    }
+};
