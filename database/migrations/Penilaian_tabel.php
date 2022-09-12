560e4eaf2f2afb1e13d94330d169d45caf3bf028
kevnlj <103430507+kevnlj@users.noreply.github.com>
2022-09-12 23:09:19 +0700

    Membuat tabel Penilaian

---
+++ database/migrations/Penilaian_tabel.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Penilaian', function (Blueprint $table) {
+            $table->int('ID_Penilaian');
+            $table->string('Nama_Penilaian');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Penilaian');
+    }
+};
