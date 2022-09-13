9f673177a37c750b597a5762fcda854f4ca2468c
alviantaa <112466435+alviantaa@users.noreply.github.com>
2022-09-13 13:04:45 +0700

    testing

--- database/migrations/Sub_CPMK.php
+++ database/migrations/Sub_CPMK.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('Sub_CPMK', function (Blueprint $table) {
             $table->int('ID_SUB_CPMK')->primary;
             $table->string('Desc');
-            $table->int('FK_ID_CPMK');
-            $table->foreign('FK_ID_CPMK')->references('ID_CPMK')->on('CPMK'); 
+           // $table->int('FK_ID_CPMK');
+            //$table->foreign('FK_ID_CPMK')->references('ID_CPMK')->on('CPMK'); 
         });
     }
 
