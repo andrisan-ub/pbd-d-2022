6bdd1bd138ba623754cb92725db416d782dd2332
alviantaa <112466435+alviantaa@users.noreply.github.com>
2022-09-19 14:21:12 +0700

    Constrains Penilaian
    tidak bisa lewat laravel, manual constrain via MySQL

--- database/migrations/10Penilaian_tabel.php
+++ database/migrations/10Penilaian_tabel.php
@@ -16,7 +16,18 @@ return new class extends Migration
         Schema::create('Penilaian', function (Blueprint $table) {
             $table->integer('ID_Penilaian')-> primary();
             $table->string('Nama_Penilaian', 10);
+            $table->integer('ID_SUB_CPMK');
+            $table->integer('ID_CPMK');
             $table->double('Bobot');
+           
+            //ini eror, gabisa reference via laravel. MANUAL lewat Mysql
+            //$table->foreign('ID_SUB_CPMK')-> references ('ID_sub_CPMK') -> on ('sub_cpmk');
+            //$table->foreign('ID_CPMK')-> references ('CPMK_ID_CPMK') -> on ('Sub_CPMK'); 
+
+            //Mysql Syntax :
+            //alter table penilaian
+            //add FOREIGN KEY (ID_SUB_CPMK) REFERENCES sub_cpmk(ID_SUB_CPMK),
+            //add FOREIGN KEY (ID_CPMK) REFERENCES sub_cpmk(CPMK_ID_CPMK);          
         });
     }
 
