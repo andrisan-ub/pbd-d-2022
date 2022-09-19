6351751ecbe560a72f5b5ce6c7a0b6c54f3b7dfa
claireYng <claraclaritayng@gmail.com>
2022-09-19 19:56:44 +0700

    Update 2022_09_12_133348_pengesahan.php

--- database/migrations/2022_09_12_133348_pengesahan.php
+++ database/migrations/2022_09_12_133348_pengesahan.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('Dosen_Pengampu')->primary;
             $table->unsignedBigInteger('Mata_Kuliah_Kode_MK')->nullable();
             $table->unsignedBigInteger('RPS_Kode_RPS')->nullable();
-            $table->string('Status_2');
+            $table->string('Status');
             $table->string('KPS');
             $table->string('KKJFD');
 
