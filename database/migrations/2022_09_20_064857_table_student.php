901d6fa717586ccb1ea185da84b55be60f470166
firdakusuma <firdakusumaaa@gmail.com>
2022-09-20 13:58:59 +0700

    Update 2022_09_20_064857_table_student.php

--- database/migrations/2022_09_20_064857_table_student.php
+++ database/migrations/2022_09_20_064857_table_student.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create ('student', function (Blueprint $table){
+            
+        });
     }
 
     /**
