ba6577508396105ac084fbc2d336b79bff2eed0f
shafaradine <shafaradina@student.ub.ac.id>
2022-09-21 15:42:18 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('intended_learning_outcome')->insert([
+            'ID_ILO' => '000111',
+            'POSITION' => '02',
+            'DESCRIPTION' => 'Mampu merancang dan mengimplementasikan solusi teknologi infromasi terintegrasi yang diperlukan untuk mewujudkan sistem yang terpadu secara efektif pada suatu organisasi',
+            'SYLLABUS_ID_SYLLABUS' => '',
+        ]);
+
+
+        DB::table('intended_learning_outcome')->insert([
+            'ID_ILO' => '000112',
+            'POSITION' => '09',
+            'DESCRIPTION' => 'Lulusan memiliki kemampuan untuk bersikap ilmiah, bekerja secara kolaboratif, memiliki sikap profesionalisme, serta mampu beradaptasi dengan baik di dalam situasi kerja kelompok maupun individu',
+            'SYLLABUS_ID_SYLLABUS' => '',
+        ]);
     }
 }
