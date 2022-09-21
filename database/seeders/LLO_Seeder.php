b6ef6aca1ce2b346965c2b270f10a94c7218071a
Naufal Ammar Rauf <naufalammarrauf7@gmail.com>
2022-09-21 15:41:57 +0700

    Membuat data seeder Lesson Learning Outcome

--- database/seeders/LLO_Seeder.php
+++ database/seeders/LLO_Seeder.php
@@ -18,5 +18,43 @@ class LLO_Seeder extends Seeder
     public function run()
     {
         //
+
+        DB::table('lesson_learning_outcome')->insert([
+            'id' => 'L1',  
+            'position' => '',
+            'description' => 'Mampu memahami konsep dasar pemrograman database dan struktur stored procedure',
+            // 'clo_id' => '',
+        ]);
+
+        DB::table('lesson_learning_outcome')->insert([
+            'id' => 'L2',  
+            'position' => '',
+            'description' => 'Mampu mengimplementasikan logika pemrograman dalam stored procedure',
+            // 'clo_id' => '',
+        ]);
+
+        DB::table('lesson_learning_outcome')->insert([
+            'id' => 'L3',  
+            'position' => '',
+            'description' => 'Mampu mengimplementasikan cursor, function, dan trigger',
+            // 'clo_id' => '',
+        ]);
+
+        DB::table('lesson_learning_outcome')->insert([
+            'id' => 'L4',  
+            'position' => '',
+            'description' => 'Mampu merancang dan mengimplementasikan dynamic sql dan mengintegrasikannya dalam aplikasi',
+            // 'clo_id' => '',
+        ]);
+
+        DB::table('lesson_learning_outcome')->insert([
+            'id' => 'L5',  
+            'position' => '',
+            'description' => 'Mampu menyampaikan hasil progres rancangan dan implementasi projek',
+            // 'clo_id' => '',
+        ]);
+
+
+
     }
 }
