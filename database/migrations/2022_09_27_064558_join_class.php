7249430c17e0d46a55709fcf3317b500e61d8d46
phik753 <89512318+phik753@users.noreply.github.com>
2022-09-27 13:53:06 +0700

    create join_class

---
+++ database/migrations/2022_09_27_064558_join_class.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('join_class', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('class_id')->notnull();
+            $table->unsignedBigInteger('user_id')->notnull();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
