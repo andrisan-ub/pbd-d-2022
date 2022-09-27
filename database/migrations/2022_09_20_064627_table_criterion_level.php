44bb123e6b2a151cf4df84b7602d1bc330399f01
fauzanzakaria <fauzanzakaria449@gmail.com>
2022-09-27 13:04:46 +0700

    Update 2022_09_20_064627_table_criterion_level.php

--- database/migrations/2022_09_20_064627_table_criterion_level.php
+++ database/migrations/2022_09_20_064627_table_criterion_level.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('criterion_level', function (Blueprint $table) {
             $table->UnsignedBigInteger('id')->notnull()->primary;
             $table->float('point')->notnull;
-            $table->char('title',255)->nullable();
+            $table->char('title')->nullable();
             $table->text('description')->nullable();
         });
     }
