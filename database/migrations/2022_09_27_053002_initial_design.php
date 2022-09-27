a1d08dfc89aa085184997f0c85f491d9df6043b4
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2022-09-27 13:53:35 +0700

    Update 2022_09_27_053002_initial_design.php

--- database/migrations/2022_09_27_053002_initial_design.php
+++ database/migrations/2022_09_27_053002_initial_design.php
@@ -13,7 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('faculty', function (Blueprint $table) {
+            $table->id()->unsigned;
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
