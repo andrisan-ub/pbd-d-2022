e234ea2749da8653f3c56cc5c9a5af363392b182
jasmineffendy <103312975+jasmineffendy@users.noreply.github.com>
2022-09-27 15:02:49 +0800

    Update Criterion_levelSeeder.php

--- database/seeders/Criterion_levelSeeder.php
+++ database/seeders/Criterion_levelSeeder.php
@@ -24,72 +24,74 @@ class DatabaseSeeder extends Seeder
     DB::table('criterion')->insert([
         'id' => 001,
         'point' => 5,
-        'title' => 'Excellent',
-        'description' => 'Mampu menjelaskan konsep yang ditanyakan dengan benar dan terperinci. Sangat menguasai materi',
+        'title' => 'Baik',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 002,
         'point' => 4,
-        'title' => 'Proficient',
-        'description' => 'Mampu menelaskan konsep yang ditanyakan dengan benar, tetapi masih ada bagian-bagia yang kurang dijabarkan',
+        'title' => 'Cukup',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 003,
         'point' => 3,
-        'title' => 'Satisfactory',
-        'description' => 'Hanya menjelaskan secara garis besar saja, masih belum menguasai materi tentang konsep yang ditanyakan',
+        'title' => 'Kurang',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 004,
         'point' => 2,
-        'title' => 'Poor',
-        'description' => 'Berusaha menjawab pertanyaan yang ditanyakan dengan konsep yang tidak ada hubungannya dengan pertanyaan',
+        'title' => 'Baik',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 005,
         'point' => 1,
-        'title' => 'Incomplete',
-        'description' => 'Tidak bisa menjawab dan terlihat sangat tidak menguasai konsep yang ditanyakan',
+        'title' => 'Cukup',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 001,
         'point' => 5,
-        'title' => 'Excellent',
-        'description' => 'Mampu mengimplementsikan tugas yang diberikan dengan sangat baik dan lancar. Sangat menguasai tools atau framework yang digunakan.',
+        'title' => 'Kurang',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 002,
         'point' => 4,
-        'title' => 'Proficient',
-        'description' => 'Mampu mengimplementasikan tugas yang diberikan dengan baik walaupun ada yang sedikit belum tuntas. Pemahaman tentang tools dan framework yang digunakan masih bisa ditingkatkan.',
+        'title' => 'Baik',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 003,
         'point' => 3,
-        'title' => 'Satisfactory',
-        'description' => 'Setengah jalan atau lebih dalam mengimplementasikan tugas yang diberikan. Menunjukkan arah pengerjaan yang benar',
+        'title' => 'Cukup',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 004,
         'point' => 2,
-        'title' => 'Poor',
-        'description' => 'Belum selesai mengimplementasikan tugas yang diberikan, tetapi ada progress yang cukup. Menunjukkan pemahaman yang kurang',
+        'title' => 'Kurang',
     ]);
 
     DB::table('criterion')->insert([
         'id' => 005,
         'point' => 1,
-        'title' => 'Incomplete',
-        'description' => 'Hampir tidak ada progress sama sekali dalam mengerjakan tugas yang diberikan',
+        'title' => 'Baik',
     ]);
 
+    DB::table('criterion')->insert([
+        'id' => 003,
+        'point' => 3,
+        'title' => 'Cukup',
+    ]);
+
+    DB::table('criterion')->insert([
+        'id' => 004,
+        'point' => 2,
+        'title' => 'Kurang',
+    ]);
+    
    }
 }
