6f7621fd8a239c087c4b1c2e6ee82747d318cc07
fauzanzakaria <fauzanzakaria449@gmail.com>
2022-09-28 22:23:27 +0700

    Update Criterion_levelSeeder.php

--- database/seeders/Criterion_levelSeeder.php
+++ database/seeders/Criterion_levelSeeder.php
@@ -21,75 +21,75 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-    DB::table('criterion')->insert([
-        'id' => 001,
-        'point' => 5,
+    DB::table('criterion_level')->insert([
+        'id' => 1,  
+        'point' => 2.5,
         'title' => 'Baik',
     ]);
 
-    DB::table('criterion')->insert([
-        'id' => 002,
-        'point' => 4,
+    DB::table('criterion_level')->insert([
+        'id' => 2,
+        'point' => 2,
         'title' => 'Cukup',
     ]);
 
-    DB::table('criterion')->insert([
-        'id' => 003,
-        'point' => 3,
-        'title' => 'Kurang',
-    ]);
-
-    DB::table('criterion')->insert([
-        'id' => 004,
-        'point' => 2,
-        'title' => 'Baik',
-    ]);
-
-    DB::table('criterion')->insert([
-        'id' => 005,
+    DB::table('criterion_level')->insert([
+        'id' => 3,
         'point' => 1,
-        'title' => 'Cukup',
+        'title' => 'Kurang',
     ]);
 
-    DB::table('criterion')->insert([
-        'id' => 001,
+    DB::table('criterion_level')->insert([
+        'id' => 4,
+        'point' => 7.5,
+        'title' => 'Baik',
+    ]);
+
+    DB::table('criterion_level')->insert([
+        'id' => 5,
         'point' => 5,
-        'title' => 'Kurang',
-    ]);
-
-    DB::table('criterion')->insert([
-        'id' => 002,
-        'point' => 4,
-        'title' => 'Baik',
-    ]);
-
-    DB::table('criterion')->insert([
-        'id' => 003,
-        'point' => 3,
         'title' => 'Cukup',
     ]);
 
-    DB::table('criterion')->insert([
-        'id' => 004,
+    DB::table('criterion_level')->insert([
+        'id' => 6,
         'point' => 2,
         'title' => 'Kurang',
     ]);
 
-    DB::table('criterion')->insert([
-        'id' => 005,
+    DB::table('criterion_level')->insert([
+        'id' => 7,
+        'point' => 2.5,
+        'title' => 'Baik',
+    ]);
+
+    DB::table('criterion_level')->insert([
+        'id' => 8,
+        'point' => 2,
+        'title' => 'Cukup',
+    ]);
+
+    DB::table('criterion_level')->insert([
+        'id' => 9,
         'point' => 1,
+        'title' => 'Kurang',
+    ]);
+
+    DB::table('criterion_level')->insert([
+        'id' => 10,
+        'point' => 2.5,
         'title' => 'Baik',
     ]);
 
-    DB::table('criterion')->insert([
-        'id' => 003,
-        'point' => 3,
+    DB::table('criterion_level')->insert([
+        'id' => 11,
+        'point' => 2,
         'title' => 'Cukup',
     ]);
 
-    DB::table('criterion')->insert([
-        'id' => 004,
-        'point' => 2,
+    DB::table('criterion_level')->insert([
+        'id' => 12,
+        'point' => 1,
         'title' => 'Kurang',
     ]);
     
