123981043db6dd47558769ba6e115bedb6a4bd25
shafaradine <shafaradina@student.ub.ac.id>
2022-09-28 14:56:04 +0700

    mengatasi conflicts

--- database/seeders/ILO_Seeder.php
+++ database/seeders/ILO_Seeder.php
@@ -6,7 +6,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
+class ILO_Seeder extends Seeder
 {
     /**
      * Seed the application's database.
