28019ceee2bdc58d684f2f5398a4a4cc4dfe3d25
Yehezkiel Imannuel <91413725+yehezkiel0@users.noreply.github.com>
2022-10-11 12:58:23 +0700

    membuat create dan read store procedure tabel rubric

--- database/migrations/2022_10_11_035753_create_rubric_store_procedure.php
+++ database/migrations/2022_10_11_035753_create_rubric_store_procedure.php
@@ -14,7 +14,7 @@ return new class extends Migration
      */
     public function up()
     {   
-        // Membuat get procedure - Yehezkiel Imannuel
+        // Membuat read procedure - Yehezkiel Imannuel - 215150701111018
         $get_procedure = "DROP PROCEDURE IF EXISTS `get_rubric_by_id`;
                 CREATE PROCEDURE  `get_rubric_by_id` (IN id_rubc int)
                 
@@ -26,18 +26,55 @@ return new class extends Migration
                 ";
 
             DB::unprepared($get_procedure);
+        
+        // Membuat create procedure - Yehezkiel Imannuel - 215150701111018
 
         $create_procedure = "DROP PROCEDURE IF EXISTS `create_rubric_by_id`;
-                CREATE PROCEDURE `create_rubric_by_id` (IN id_rubc int, IN asg_id int, IN nme varchar(1024))
+                CREATE PROCEDURE `create_rubric_by_id` (
+                    IN id_rubc int, 
+                    IN asg_id int, 
+                    IN new_title varchar(1024), 
+                    new_create_at datetime,
+                    new_update_at datetime
+                )
 
                 BEGIN 
-                insert into rubric
-                values(id_rubc, asg_id, nme);
+                INSERT INTO rubric(id, assignment_plan_id, title)
+                VALUES(id_rubc, asg_id, new_title);
                 END;
 
         
                 ";
             DB::unprepared($create_procedure);
+
+        $update_procedure = "DROP PROCEDURE IF EXISTS `update_rubric_by_id`;
+                CREATE PROCEDURE `update_rubric_by_id` (
+                    IN id_rubc int, 
+                    IN new_title varchar(1024)
+                )
+
+                BEGIN 
+                UPDATE rubric SET title = new_title
+                WHERE id = id_rubc;
+                END;
+
+        
+                ";
+            DB::unprepared($update_procedure);
+
+        $delete_procedure = "DROP PROCEDURE IF EXISTS `delete_rubric_by_id`;
+                CREATE PROCEDURE `delete_rubric_by_id` (
+                    id_rubc int
+                )
+
+                BEGIN 
+                DELETE FROM rubric
+                WHERE id = id_rubc;
+                END;
+
+        
+                ";
+            DB::unprepared($delete_procedure);
     }
 
     /**
