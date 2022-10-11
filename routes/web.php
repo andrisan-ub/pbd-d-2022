c2e59fc6cf258b4b055d7b2eb453e2e58d9531bd
auliasanggraini <anggirhama12@gmail.com>
2022-10-11 20:17:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/read', function (){
-    $course = DB::select('call viewCourse()');
-    dump($course);
-});
