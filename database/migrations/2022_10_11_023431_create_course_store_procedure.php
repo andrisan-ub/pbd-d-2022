932263723c7d35b21862d4bd0bcf7c7fa88e696d
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2022-10-11 10:28:23 +0700

    memperbaiki store procedure study_program

--- database/migrations/2022_10_11_023431_create_course_store_procedure.php
+++ database/migrations/2022_10_11_023431_create_course_store_procedure.php
@@ -36,7 +36,7 @@ return new class extends Migration
         $procedure_create = "DROP PROCEDURE IF EXISTS `create_study_program_by_study_program_id`;
             CREATE PROCEDURE `create_study_program_by_study_program_id` (new_id int, new_department_id int, new_name varchar(255))
             BEGIN
-            INSERT INTO study_program
+            INSERT INTO faculty
             VALUES(new_id, new_department_id, new_name);
             END;";
   
@@ -46,7 +46,7 @@ return new class extends Migration
         $procedure_delete = "DROP PROCEDURE IF EXISTS `delete_study_program_by_study_program_id`;
             CREATE PROCEDURE `delete_study_program_by_study_program_id` (new_id int)
             BEGIN
-            DELETE FROM study_program
+            DELETE FROM faculty
             WHERE id = new_id;
             END;";
   
@@ -60,6 +60,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('study_program_store_procedure');
+        Schema::dropIfExists('course_store_procedure');
     }
 };
