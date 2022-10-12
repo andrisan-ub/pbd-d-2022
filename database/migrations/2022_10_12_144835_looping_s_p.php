d2e7f08c442cc79cf59b90178e3b15631078ff18
kevnlj <103430507+kevnlj@users.noreply.github.com>
2022-10-13 00:49:12 +0700

    Membuat Looping procedure untuk tabel criterion_level

--- database/migrations/2022_10_12_144835_looping_s_p.php
+++ database/migrations/2022_10_12_144835_looping_s_p.php
@@ -13,7 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        // LOOPING PROCEDURE - Kevin Leonardo Julianto - 215150701111039
+        // Loop untuk menampilkan tabel criterion_level dengan point yang lebih dari atau sama dengan 4.25
+        $procedure_loop = "DROP PROCEDURE IF EXISTS `kelompok5_loop_criterion_level`;
+                CREATE PROCEDURE `kelompok5_loop_criterion_level` ()
+                BEGIN
+                    DECLARE x float;
+                    SET x = 4.25; 
+                    WHILE x <= 10 DO
+                    SELECT * FROM criterion_level
+                    WHERE point = x;
+                    SET x = x + 0.25;
+                    END WHILE;
+                END;";
+
+        DB::unprepared($procedure_loop);
+
+
+
     }
 
     /**
