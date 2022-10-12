4236a92c72d266112e58d092706b8f664f9fc931
fairuznp <74838107+Fairuz5@users.noreply.github.com>
2022-10-12 17:35:40 +0700

    Update 2022_10_12_091331_kelompok3_create_clo_stored_procedure_table.php

--- database/migrations/2022_10_12_091331_kelompok3_create_clo_stored_procedure_table.php
+++ database/migrations/2022_10_12_091331_kelompok3_create_clo_stored_procedure_table.php
@@ -15,25 +15,44 @@ return new class extends Migration
     public function up()
     {
       // Procedure create - Fairuz - 215150701111023
-      $procedure_create_new = "DROP PROCEDURE IF EXISTS 'create_clo';
-      CREATE PROCEDURE 'kelompok3_create_clo'(IN 'id' BIGINT(20) UNSIGNED, IN 'ilo_id' BIGINT(20) UNSIGNED, IN 'position' INT(11), IN 'description' TEXT)
-      BEGIN
-      INSERT INTO course_learning_outcome VALUES(IN_id, IN_ilo_id, IN_position, IN_description);
-      END;";
-
-      DB::unprepared($procedure_create_new);
+      $create_procedure = "DROP PROCEDURE IF EXISTS `kelompok3_create_clo`;
+        CREATE PROCEDURE `kelompok3_create_clo` (
+	
+            in_id bigint (20),
+            in_ilo_id bigint (20),
+            in_position int (11),
+            in_description text
+            
+        )
+        BEGIN
+        
+            insert into lesson_learning_outcomes
+            values(in_id, in_ilo_id, in_position, in_description);
+        
+        
+        END;";
+      
+        \DB::unprepared($create_procedure);
     
 
        //procedure update - Aulia S.A - 215150701111045
-       $procedure_update_by_id = "DROP PROCEDURE IF EXISTS 'update_clo';
-       CREATE PROCEDURE 'update_clo' (IN 'new_id' int, IN new_ilo_id BIGINT(20), IN new_position INT(11), IN new_description TEXT)
-       BEGIN
-       UPDATE course_learning_outcome 
-       SET id = new_id, SET ilo = new_ilo_id, SET position = new_position, SET description = new_description
-       WHERE id = new_id;
-       END;";
-
-       DB::unprepared($procedure_update_by_id);
+       $update_procedure = "DROP PROCEDURE IF EXISTS `kelompok3_create_clo`;
+        CREATE PROCEDURE `kelompok3_create_clo` (
+	
+            new_id bigint (20),
+            new_ilo_id bigint (20),
+            new_position int (11),
+            new_description text
+            
+        )
+        BEGIN
+            UPDATE course_learning_outcomes SET id = new_id WHERE id = new_id;
+            UPDATE course_learning_outcomes SET ilo_id = new_ilo_id WHERE id = new_id;
+            UPDATE course_learning_outcomes SET position = new_position WHERE id = new_id;
+            UPDATE course_learning_outcomes SET description = new_description WHERE id = new_id;
+        END;";
+      
+        \DB::unprepared($update_procedure);
 
   }
 
