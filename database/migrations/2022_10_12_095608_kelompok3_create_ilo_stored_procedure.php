0c3856f3bfba595ce8b8d08fd06217801012e1b1
shafaradine <shafaradina@student.ub.ac.id>
2022-10-12 18:42:00 +0700

    Update 2022_10_12_095608_kelompok3_create_ilo_stored_procedure.php

--- database/migrations/2022_10_12_095608_kelompok3_create_ilo_stored_procedure.php
+++ database/migrations/2022_10_12_095608_kelompok3_create_ilo_stored_procedure.php
@@ -13,14 +13,34 @@ return new class extends Migration
      */
     public function up()
     {
-        $procedure_create_new = "DROP PROCEDURE IF EXISTS `create_ilo`;
-       CREATE PROCEDURE `create_ilo` (IN id BIGINT(20) UNSIGNED, IN position INT, IN description TEXT, IN syllabus_id BIGINT(20) UNSIGNED)
+        // CREATE by NAUFAL AR - 215150701111014
+        $procedure_create = "DROP PROCEDURE IF EXISTS 'create_ilo';
+        CREATE PROCEDURE 'create_ilo' (
+            cr_id BIGINT(20) UNSIGNED,
+            cr_syllabus_id BIGINT(20) UNSIGNED,
+            cr_position INT,
+            cr_description TEXT
+        )
+        BEGIN
+        INSERT INTO intended_learning_outcome
+            VALUES(id, position, description, syllabus);
+        END";
 
-       BEGIN
-       INSERT INTO intended_learning_outcome VALUES(id, position, description, syllabus);
-       END;";
-
-       DB::unprepared($procedure_create_new);
+        // UPDATE by SHAFA AF - 215150700111057
+        $procedure_update = "DROP PROCEDURE IF EXSIST 'update_ilo';
+        CREATE PROCEDURE 'kelompok3_update_ilo'(
+            up_id BIGINT(20) UNSIGNED,
+            up_syllabus_id BIGINT(20) UNSIGNED,
+            up_position INT(20),
+            up_description text
+        )
+        BEGIN
+            UPDATE intended_learning_outcome set
+            syllabus_id = up_syllabus_id,
+            position = up_position,
+            description = up_description,
+            WHERE id = up_id;
+        END";
     }
 
     /**
