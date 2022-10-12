312c875394f7e585093c20629b27037686b9c7e1
kevnlj <103430507+kevnlj@users.noreply.github.com>
2022-10-12 22:37:52 +0700

    Membuat create procedure untuk tabel syllabus

--- database/migrations/2022_10_12_144604_create_s_p.php
+++ database/migrations/2022_10_12_144604_create_s_p.php
@@ -13,6 +13,28 @@ return new class extends Migration
      */
     public function up()
     {
+        // CREATE PROCEDURE - Kevin Leonardo Julianto - 215150701111039
+        // Create untuk menambah baris data pada tabel syllabus
+        $procedure_create = "DROP PROCEDURE IF EXISTS `kelompok5_create_syllabus`;
+        CREATE PROCEDURE `kelompok5_create_syllabus` (
+
+            in_id bigint(20),
+            in_course_id bigint(20),
+            in_title varchar(255),
+            in_author text,
+            in_head_of_study_program varchar(512)
+
+        )
+        BEGIN
+
+        INSERT INTO syllabus
+            VALUES(in_id, in_course_id, in_title, in_author, in_head_of_study_program);
+
+        END;";
+
+        DB::unprepared($procedure_create);
+
+
         
     }
 
