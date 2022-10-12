eb9577bf0d1da0000ebea019cbb588b6c7ca4f39
alviantaa <112466435+alviantaa@users.noreply.github.com>
2022-10-13 02:27:52 +0700

    SP Condition untuk membandingkan civitas

--- database/migrations/2022_10_12_144825_condition_s_p.php
+++ database/migrations/2022_10_12_144825_condition_s_p.php
@@ -13,7 +13,28 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        //CONDITION PROCEDURE - Alvianta Dwi Putra - 215150700111053
+        //condition untuk menampilkan perbandingan antara dosen dan mahasiswa
+        $procedure_create = "DROP PROCEDURE IF EXISTS `compare_civitas`;
+        CREATE PROCEDURE compare_civitas (
+            )
+            BEGIN
+            DECLARE COUNT1 INT;
+            DECLARE COUNT2 INT;
+            set COUNT1 = (SELECT COUNT(*) FROM users us
+            WHERE NOT EXISTS (SELECT* FROM student_info st WHERE us.id=st.id));
+            set COUNT2 = (SELECT COUNT(*) FROM student_info);
+            if (COUNT1)>(COUNT2) then
+            SET @print ='Dosen lebih banyak dari Mahasiswa';
+            elseif (COUNT1)<(COUNT2) then
+            SET @print ='Mahasiswa lebih banyak dari Dosen';
+            ELSE
+            SET @print ='Jumlah dosen dan mahasiswa sama';
+            END if;
+            SET @tampil = CONCAT('Dosen: ', COUNT1, ' | ', ' Mahasiswa: ', COUNT2, ' | ', @print);
+            SELECT @tampil;
+            END;";
+        DB::unprepared($procedure_create);
     }
 
     /**
