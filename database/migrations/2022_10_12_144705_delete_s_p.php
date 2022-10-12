11b7920efb8aac5d3af4c48c380c50ed0669fcc0
Ramaadit18 <ramaawp10@gmail.com>
2022-10-13 03:42:31 +0700

    SP delete users
    SP untuk menghapus data pada table users berdasarkan id ke-x

--- database/migrations/2022_10_12_144705_delete_s_p.php
+++ database/migrations/2022_10_12_144705_delete_s_p.php
@@ -13,7 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        // DELETE PROCEDURE - Rama Adhitya Widodo Putra - 215150700111052
+        // Prosedur untuk data pada tabel users berdasarkan id ke-x
+        $procedure_delete = "DROP PROCEDURE IF EXISTS `kelompok5_delete_users`;
+        CREATE PROCEDURE `kelompok5_delete_users` (
+            IN x INT
+        )
+        BEGIN
+            DELETE FROM users
+            WHERE id = x;
+        END;";
+
+        DB::unprepared($procedure_delete);
     }
 
     /**
