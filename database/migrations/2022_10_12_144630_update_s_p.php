addcb07f858ec56598139ccbec8d87ee24296e07
kevnlj <103430507+kevnlj@users.noreply.github.com>
2022-10-12 22:38:07 +0700

    Membuat update procedure untuk tabel syllabus

--- database/migrations/2022_10_12_144630_update_s_p.php
+++ database/migrations/2022_10_12_144630_update_s_p.php
@@ -13,7 +13,29 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        // UPDATE PROCEDURE - Kevin Leonardo Julianto - 215150701111039
+        // Update untuk mengubah data pada tabel syllabus berdasarkan id
+        $procedure_update = "DROP PROCEDURE IF EXISTS `kelompok5_update_syllabus`;
+        CREATE PROCEDURE `kelompok5_update_syllabus` (
+
+            IN p_id bigint(20),
+            IN up_course_id bigint(20),
+            IN up_title varchar(255),
+            IN up_author text,
+            IN up_head_of_study_program varchar(512)
+
+        )
+        BEGIN
+
+        UPDATE syllabus SET course_id = up_course_id, title = up_title, author = up_author, head_of_study_program = up_head_of_study_program
+        WHERE id = p_id;
+
+        END;";
+
+        DB::unprepared($procedure_update);
+
+
+        
     }
 
     /**
