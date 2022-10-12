769a1a3d1eef1942253ed4b87d9e94656a43bdd6
putriraiss <putridaliana@gmail.com>
2022-10-12 19:33:55 +0700

    .

--- database/migrations/2022_10_12_100323_Kelompok2_create_faculty_table.php
+++ database/migrations/2022_10_12_100323_Kelompok2_create_faculty_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -14,10 +15,13 @@ return new class extends Migration
     public function up()
     {
         // Procedure create - Putri Daliana - 215150700111043
-        $procedure_create_new= "DROP PROCEDURE IF EXISTS 'create_faculty';
-        CREATE PROCEDURE 'kelompok2_create_faculty'(IN 'id' BIGINT(20) UNSIGNED, IN 'name' VARCHAR(45))
+        $procedure_create_new = "DROP PROCEDURE IF EXISTS 'kelompok2_create_faculty';
+        CREATE PROCEDURE 'kelompok2_create_faculty' (
+           in_id bigint(20),
+            in_name varchar(45)
+
         BEGIN 
-        INSERT INTO faculty VALUES (IN_id, IN_name);
+        INSERT INTO faculty VALUES (in_id, in_name);
         END;";
 
         DB::unprepared($procedure_create_new);
