17dc1836b24d16feeba52ef8905d8fd92ac088e1
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2022-10-13 11:28:35 +0700

    membuat stored procedure ifelse dan loop

--- database/migrations/2022_10_13_032544_kelompok_1_stored_procedure_read.php
+++ database/migrations/2022_10_13_032544_kelompok_1_stored_procedure_read.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         //procedure read criteria and criteria levels - Muchammad Saifurrijaal - 215150701111006
-        $procedure_create = "DROP PROCEDURE IF EXISTS `kelompok_1_read_criterias_and_criterias_level`;
+        $procedure_read = "DROP PROCEDURE IF EXISTS `kelompok_1_read_criterias_and_criterias_level`;
             CREATE PROCEDURE `kelompok_1_read_criterias_and_criterias_level` ()
             BEGIN
             SELECT c.title AS criteria_title, cl.title AS criteria_levels_title
@@ -22,10 +22,10 @@ return new class extends Migration
             JOIN criteria_levels cl ON c.id = cl.criteria_id;
             END;";
 
-        \DB::unprepared($procedure_create);
+        \DB::unprepared($procedure_read);
 
         //procedure read student_user and course - Muchammad Saifurrijaal - 215150701111006
-        $procedure_create = "DROP PROCEDURE IF EXISTS `kelompok_1_read_student_user_and_course`;
+        $procedure_read = "DROP PROCEDURE IF EXISTS `kelompok_1_read_student_user_and_course`;
             CREATE PROCEDURE `kelompok_1_read_student_user_and_course` ()
             BEGIN
             SELECT u.name AS student_name, c.name AS course_name
@@ -36,7 +36,7 @@ return new class extends Migration
             JOIN courses c ON c.id = cc.course_id
             WHERE u.role = 'student';
             END;";
-        \DB::unprepared($procedure_create);
+        \DB::unprepared($procedure_read);
     }
 
     /**
