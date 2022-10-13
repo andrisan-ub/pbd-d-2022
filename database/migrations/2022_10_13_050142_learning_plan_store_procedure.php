0288010fa3513664f85efe2cce9721ffb2484706
fathirizqiii <ekifathi@Gmail.com>
2022-10-13 14:17:19 +0700

    pbd

--- database/migrations/2022_10_13_050142_learning_plan_store_procedure.php
+++ database/migrations/2022_10_13_050142_learning_plan_store_procedure.php
@@ -15,17 +15,25 @@ return new class extends Migration
     {
     $create_procedure = "DROP PROCEDURE IF EXISTS
     `create_learning_plan_by_id`;
-            CREATE PROCEDURE `create_learning_plan_by_id` (
-                         id_learn_plan int,
-                         new_syllabus_id int,
-                         new_week_number int,
-                         new_llo_id int,
-                         new_study_material text,
-                         new_learn_method text,
-                         new_est_time varchar(1024),
-                         new_created_at int,
-                         new_updated_at int )
-                         
+        CREATE PROCEDURE `create_learning_plan_by_id` (
+            id_learn_plan int,
+            new_syllabus_id int,
+            new_week_number int,
+            new_llo_id int,
+            new_study_material text,
+            new_learn_method text,
+            new_est_time varchar(1024),
+            new_created_at int,
+            new_updated_at int )
+
+        BEGIN
+        INSERT INTO course_class(
+          id, syllabus_id, week_number, llo_id, study_material, learning_method, estimated_time, created_at, updated_at)
+        VALUES(
+          d_learn_plan, new_syllabus_id, new_week_number, new_llo_id, new_study_material, new_learn_method, new_est_time, new_created_at, new_updated_at);
+        END;
+
+        ";
     }
 
     /**
