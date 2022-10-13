cc8a8931cdaeb0ccbd6cdc15e1e3f68dfb268653
Redomeire <redomeire2@gmail.com>
2022-10-13 11:04:06 +0700

    change criterias stored procedure name

--- database/migrations/2022_10_13_025551_kelompok_1_stored_procedure_criterias.php
+++ database/migrations/2022_10_13_025551_kelompok_1_stored_procedure_criterias.php
@@ -15,28 +15,28 @@ return new class extends Migration
     {
         
        //procedure get by id - Redomeire - 215150701111032
-       $procedure_get_by_id = "DROP PROCEDURE IF EXISTS `kelompok_1_get_criterion_by_criterion_id`;
-       CREATE PROCEDURE `kelompok_1_get_criterion_by_criterion_id` (IN new_id int)
+       $procedure_get_by_id = "DROP PROCEDURE IF EXISTS `kelompok_1_get_criterias_by_criterias_id`;
+       CREATE PROCEDURE `kelompok_1_get_criterias_by_criterias_id` (IN new_id int)
        BEGIN
-       SELECT * FROM criterion WHERE id = new_id;
+       SELECT * FROM criterias WHERE id = new_id;
        END;";
 
        DB::unprepared($procedure_get_by_id);
 
         //procedure create - Redomeire - 215150701111032
-       $procedure_create_new = "DROP PROCEDURE IF EXISTS `kelompok_1_create_criterion`;
-       CREATE PROCEDURE `kelompok_1_create_criterion` (IN new_id int, IN new_rubric_id int, IN new_llo_id int, IN new_title varchar(1024), IN new_description varchar(1024), IN new_max_point double(8,2), new_created_at datetime, new_updated_at datetime)
+       $procedure_create_new = "DROP PROCEDURE IF EXISTS `kelompok_1_create_criterias`;
+       CREATE PROCEDURE `kelompok_1_create_criterias` (IN new_id int, IN new_rubric_id int, IN new_llo_id int, IN new_title varchar(1024), IN new_description varchar(1024), IN new_max_point double(8,2), new_created_at datetime, new_updated_at datetime)
        BEGIN
-       INSERT INTO criterion VALUES(new_id, new_rubric_id, new_llo_id, new_title, new_description, new_max_point, new_created_at, new_updated_at);
+       INSERT INTO criterias VALUES(new_id, new_rubric_id, new_llo_id, new_title, new_description, new_max_point, new_created_at, new_updated_at);
        END;";
 
        DB::unprepared($procedure_create_new);
 
         //procedure update - Redomeire - 215150701111032
-       $procedure_update_by_id = "DROP PROCEDURE IF EXISTS `kelompok_1_update_criterion`;
-       CREATE PROCEDURE `kelompok_1_update_criterion` (IN new_id int, IN new_title varchar(1024), IN new_description varchar(1024), IN new_max_point float)
+       $procedure_update_by_id = "DROP PROCEDURE IF EXISTS `kelompok_1_update_criterias`;
+       CREATE PROCEDURE `kelompok_1_update_criterias` (IN new_id int, IN new_title varchar(1024), IN new_description varchar(1024), IN new_max_point float)
        BEGIN
-       UPDATE criterion 
+       UPDATE criterias
        SET title = new_title, SET description = new_description, SET max_point = new_max_point
        WHERE id = new_id;
        END;";
@@ -44,10 +44,10 @@ return new class extends Migration
        DB::unprepared($procedure_create_new);
 
        //procedure delete by id - Redomeire - 215150701111032
-       $procedure_delete = "DROP PROCEDURE IF EXISTS `kelompok_1_delete_criterion_by_criterion_id`;
-       CREATE PROCEDURE `kelompok_1_delete_criterion_by_criterion_id` (IN new_id int)
+       $procedure_delete = "DROP PROCEDURE IF EXISTS `kelompok_1_delete_criterion_by_criterias_id`;
+       CREATE PROCEDURE `kelompok_1_delete_criterion_by_criterias_id` (IN new_id int)
        BEGIN
-       DELETE FROM criterion WHERE id = new_id;
+       DELETE FROM criterias WHERE id = new_id;
        END;";
 
        DB::unprepared($procedure_delete);
