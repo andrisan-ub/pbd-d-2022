d8e6d7e71ce72ad2a8b91243c618204080e257ed
isntchar <diandra.charisa@student.ub.ac.id>
2022-10-15 16:09:39 +0700

    Update 2022_10_15_080836_create_kelompok2_faculties_table.php

--- database/migrations/2022_10_15_080836_create_kelompok2_faculties_table.php
+++ database/migrations/2022_10_15_080836_create_kelompok2_faculties_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,10 +14,34 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('kelompok2_faculties', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        // Procedure create - Putri Daliana - 215150700111043
+        $create_procedure = "DROP PROCEDURE IF EXISTS `kelompok2_create_faculties`;
+        CREATE PROCEDURE `kelompok2_create_faculties` (
+
+            in_id bigint (20),
+            in_name varchar (45)
+            
+        )
+        BEGIN
+            insert into faculties VALUES (in_id, in_name);
+        END;";
+    
+        DB::unprepared($create_procedure);
+    
+        // Procedure update - Diandra Charisa - 215150700111032
+        $update_procedure = "DROP PROCEDURE IF EXISTS `kelompok2_create_faculties`;
+        CREATE PROCEDURE `kelompok2_create_faculties` (
+ 
+            new_id bigint (20),
+            new_name bigint (45)
+            
+        )
+        BEGIN
+            UPDATE kelompok2_create_faculties SET id = new_id WHERE id = new_id;
+            UPDATE kelompok2_create_faculties SET name = new_name WHERE id = new_id;
+        END;";
+      
+        DB::unprepared($update_procedure);
     }
 
     /**
