b3743a8e8f239ed2da582799d40c17e7317734d0
claireYng <claraclaritayng@gmail.com>
2022-12-04 10:37:07 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(FakeDataSeeder::class);
+        DB::table('presensi')->insert([
+            `ID_Presensi`=>1,2,3,4,
+            `Nama_Mahasiswa`=>'Ulla Hubbard','Josephine Peterseri','Rowan Harrington', 'Lani Vang',
+            `NIM`=>'2151','2152','2153','2154',
+            `ID_Class`=>1,1,1,1,
+            `Class`=>'A - PBD','B - PBD','C - PBD','D - PBD',
+            `Waktu_Kehadiran`=>NOW(),NOW(),NOW(),NOW(),
+            `Hadir`=>14,13,11,7,
+            `Alpha`=>0,1,1,3
+            `Izin`=>0,0,1,1
+            `Sakit`=>0,0,1,3
+            `Total_Pertemuan`=>14,14,14,14,
+            `Presentase`=>100,93,79,50
+        ]);
     }
 }
