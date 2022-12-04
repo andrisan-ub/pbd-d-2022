eb695020a9c45d2dc3704a5c1bcd910da2fc348a
marsyanadia <deswitamn@student.ub.ac.id>
2022-12-04 22:30:51 +0700

    membuat migration dan mengisi seeder

---
+++ database/migrations/2022_12_04_150347_create_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assignments_id')->constrained('assignments');
+            $table->enum('jenis_notifikasi', array('reminder', 'announcement'))->nullable();
+            $table->text('judul_notifikasi')->nullable();
+            $table->text('pesan_notifikasi')->nullable();
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,40 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(FakeDataSeeder::class);
+
+        DB::table('notifications')->insert([
+            'id' => '1',  
+            'assignments_id' => '1',
+            'jenis_notifikasi' => 'reminder',
+            'judul_notifikasi'=> 'ADSI Modul 10',
+            'pesan_notifikasi'=>'H-1 deadline',
+            'created_at' =>  \Carbon\Carbon::now(), 
+            'updated_at' => \Carbon\Carbon::now(), 
+            
+        ]);
+
+        DB::table('notifications')->insert([
+            'id' => '2',  
+            'assignments_id' => '2',
+            'jenis_notifikasi' => 'announcement',
+            'judul_notifikasi'=> 'Modul 11',
+            'pesan_notifikasi'=>'PAW Modul 11 deadline 12/12/2021',
+            'created_at' =>  \Carbon\Carbon::now(), 
+            'updated_at' => \Carbon\Carbon::now(), 
+            
+        ]);
+
+        DB::table('notifications')->insert([
+            'id' => '3',  
+            'assignments_id' => '3',
+            'jenis_notifikasi' => 'reminder',
+            'judul_notifikasi'=> 'PBD Modul 9',
+            'pesan_notifikasi'=>'H-1 Deadline',
+            'created_at' =>  \Carbon\Carbon::now(), 
+            'updated_at' => \Carbon\Carbon::now(), 
+            
+        ]);
+
+
     }
 }
