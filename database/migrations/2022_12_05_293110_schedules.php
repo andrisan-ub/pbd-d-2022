78805cec9630fcf1a9f4df956b2cf05bd6601ac5
marcelkelvinn <marcelkelvin75@student.ub.ac.id>
2022-12-05 22:46:45 +0700

    Membuat SP create dan read untuk schedule
    Membuat SP create dan read untuk schedule

--- database/migrations/2022_12_05_293110_schedules.php
+++ database/migrations/2022_12_05_293110_schedules.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -22,7 +23,36 @@ return new class extends Migration
             $table->time('start_time', $precision = 0);
             $table->time('end_time', $precision = 0);
         });
+        
+        // SP - Create Schedule
+        $procedure_create = "DROP PROCEDURE IF EXISTS `create_schedule`;
+        CREATE PROCEDURE `create_schedule` (
+            id bigint(20),
+            course_class_id bigint(20),
+            rooms_id bigint(20),
+            teacher varchar(255),
+            day varchar(255),
+            start_time time,
+            end_time time)
+
+        )
+        BEGIN
+        INSERT INTO schedule
+            VALUES(id, course_class_id, rooms_id, teacher, day, start_time ,end_time);
+        END;";
+
+        DB::unprepared($procedure_create);
+
+        // SP - Read procedure
+        $procedure_read = "DROP PROCEDURE IF EXISTS `read_schedule`;
+        CREATE PROCEDURE `read_schedule`()
+        BEGIN
+            SELECT * FROM schedule;
+        END;";
+            
+        DB::unprepared($procedure_read);
     }
+    
 
     /**
      * Reverse the migrations.
