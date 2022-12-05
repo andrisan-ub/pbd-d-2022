0802527a81e2358c13748bbf7e702aec300f84a3
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2022-12-05 21:34:20 +0700

    tewur ngenteni

--- database/migrations/2022_12_03_121118_final_project.php
+++ database/migrations/2022_12_03_121118_final_project.php
@@ -40,6 +40,14 @@ return new class extends Migration
                 SELECT * FROM discuss_forums WHERE is_answered = true;
                 END;");
 
+        DB::unprepared("DROP PROCEDURE IF EXISTS confirm_answer;
+            CREATE PROCEDURE confirm_answer(new_id INT, new_status BOOLEAN)
+            BEGIN
+                UPDATE answers
+                    SET is_selected = new_status
+                WHERE id = new_id;
+            END;");        
+
         DB::unprepared("DROP PROCEDURE IF EXISTS count_discuss_is_answered;
                 CREATE PROCEDURE count_discuss_is_answered(OUT total_question DOUBLE)
                 BEGIN
@@ -71,6 +79,33 @@ return new class extends Migration
                 WHERE answers.is_selected = 1;
         END
         ');
+    
+    DB::unprepared('
+        DROP TRIGGER IF EXISTS delete_status;
+        CREATE TRIGGER delete_status AFTER DELETE ON answers FOR EACH ROW
+        BEGIN
+            UPDATE discuss_forums
+            INNER JOIN answers
+            ON answers.discuss_forum_id = discuss_forums.id
+                SET discuss_forums.is_answered = 0
+                WHERE answers.is_selected = 1;
+        END                        
+        ');
+
+
+    DB::unprepared('
+        DROP PROCEDURE IF EXISTS sp_update_discuss_answer;
+        CREATE PROCEDURE sp_update_discuss_answer(
+            IN TABEL VARCHAR (50),
+            IN PK VARCHAR (50),
+            IN DATA_PK VARCHAR (50),
+            IN DATA TEXT
+        )
+        BEGIN 
+           SET @SQL=CONCAT("UPDATE ", TABEL," SET ", DATA, " WHERE ", PK, "=",  DATA_PK);
+           PREPARE STAT FROM @SQL;
+           EXECUTE STAT;
+    END');    
                        
     }
 
