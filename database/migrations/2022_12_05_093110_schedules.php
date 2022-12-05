b9f50d0a0df349972d8122cfa278c630fdbcebbd
A. Fauzi <100508741+Aither-24@users.noreply.github.com>
2022-12-05 17:01:22 +0700

    Create 2022_12_05_093110_schedules.php

---
+++ database/migrations/2022_12_05_093110_schedules.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_class_id')->constrained('course_classes');
+            $table->string('teacher');
+            $table->string('day');
+            $table->times('start_time', $precision = 0);
+            $table->times('end_time', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
