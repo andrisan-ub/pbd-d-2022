02fcfbd90ad57c08dc900f6061c5c2e372f90116
abiibila <90436868+abiibila@users.noreply.github.com>
2022-12-05 18:27:37 +0700

    membuat trigger response

---
+++ database/migrations/2022_12_05_111238_create_trigger_response_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       DB::unprepared('
+            DROP TRIGGER IF EXISTS trigger_response;
+            CREATE TRIGGER trigger_response
+            AFTER UPDATE ON response
+            FOR EACH ROW
+            UPDATE notification 
+	            SET response = new.response
+	            WHERE response_id = new.id 
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trigger_response');
+    }
+};
