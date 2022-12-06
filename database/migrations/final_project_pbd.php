d57d552f9f3a0ccda4281604c2fc48962aa5c9c4
fathiyarn <inifathiyyah@gmail.com>
2022-12-06 10:02:24 +0700

    revisi function

--- database/migrations/final_project_pbd.php
+++ database/migrations/final_project_pbd.php
@@ -153,16 +153,16 @@ return new class extends Migration
 
 
         //Function - Menampilkan banyak/jumlah jawaban survei dari user
-        $stored_function = "DROP FUNCTION IF EXISTS 'get_jumSubmit';
-        CREATE FUNCTION 'get_jumSubmit'()
-        RETURNS int(11)
-        BEGIN
-            DECLARE submitted INT;
-            SELECT COUNT(id)
-            INTO submitted
-            FROM user_answers;
-	        RETURN submitted;
-        END ";
+        $stored_function = "DROP PROCEDURE IF EXISTS get_jumSubmit;
+        CREATE FUNCTION `get_jumSubmit`() 
+        RETURNS INT(11)
+        BEGIN 
+            DECLARE submitted INT; 
+            SELECT COUNT(id) 
+            INTO submitted 
+            FROM user_answers; 
+            RETURN submitted; 
+        END;";
 
         DB::unprepared($stored_function);
     }
